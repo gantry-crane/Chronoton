bb187884c5ce87492e951eed6216ce4464da02de
Tim Kächele <tim.kaechele@dm.de>
2019-04-24 10:11:23 +0200

    Add logging to chronoton executer

--- lib/chronoton/chronoton_executer.rb
+++ lib/chronoton/chronoton_executer.rb
@@ -1,9 +1,21 @@
+require 'logger'
+require 'json'
 module Chronoton
   class ChronotonExecuter
     def call(yaml_file_path, url)
+      logger.debug("Reading yaml file #{yaml_file_path}")
       plan = PlanYamlFileReader.new(yaml_file_path).load!
+
       desired_capacity = plan.currently_desired_capacity(Time.new)
-      ForemanNotifier.new(url).notify(desired_capacity)
+      logger.info("Desired Capacity for #{Time.new}: #{desired_capacity}")
+
+      response = ForemanNotifier.new(url).notify(desired_capacity)
+
+      logger.info("Foreman was notified: #{JSON.generate(response)}")
+    end
+
+    def logger
+      @logger ||= Logger.new(STDOUT)
     end
   end
 end
